a802df3f621f0e9c8bb50b1423713d8d559999c8
JWofford <wofford.sj@gmail.com>
2025-10-15 13:40:26 -0500

    password validator created

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
+//password validator
+
 const readlineSync = require('readline-sync');
 
-let userName = readlineSync.question('What is your name? ');
-let numInput = readlineSync.question('Pick a number between 0 and ' + [userName.length - 1 ]+ ': ');
-console.log("The character in your name at position " + numInput + " is " + userName[numInput] + ".");
+//check if a string contains at least one capital letter.
+function containsCapital(passwordInput){
+    for(const char of passwordInput){
+        if (char === char.toUpperCase()){
+           return true; //capital letter found, exit loop
+        } 
+    }
+    return false; //no capital letter found
+}
 
-//Should add validation to make sure the user inputs a number within the range and also not a letter.
+//check if a string contains at least one number.
+function containsNumber(passwordInput){
+    for(const char of passwordInput){
+        if(!isNaN(char)){
+            return true; //had to look up isNan.  number found, exit loop
+        }
+    }
+    return false; //no number found
+}
+
+
+//prompt the user for a password
+//check to make sure the password is at least 8 characters long
+//check to make sure it contains at least one capital letter
+//check to make sure it contains at least one number.
+
+function passwordValidation(){
+    let password;
+    do{
+         password = readlineSync.question('Please enter a valid password:  ');
+    
+    }while (password.length<8 || containsCapital(password)!== true || containsNumber(password)!== true);
+ 
+    console.log ("Your password is valid");
+}
+
+passwordValidation();
